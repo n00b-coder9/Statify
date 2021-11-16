5da7143c5c56ca3a355a7645c049767d920dffa0
n00b-coder9 <faizansari123z@gmail.com>
2021-11-16 19:57:22 +0530

    Added cross origin support

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 from flask import Flask, request, render_template, send_file
 
 from spotify_test import playlist_stats
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/")
+@cross_origin()
 def hello_word():
     return render_template("index.html")
 
 
 @app.post("/stats")
+@cross_origin()
 def playlistStats():
     playlist = request.form["nm"]
     playlist_stats(playlist)
